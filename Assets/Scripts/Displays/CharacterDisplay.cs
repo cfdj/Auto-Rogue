fdcd5765b98425c58a7c31aa904a43ae390c2095
cfdj <callumderry@gmail.com>
2020-09-25 22:05:59 +0100

    Drag and Drop!

--- Assets/Scripts/Displays/CharacterDisplay.cs
+++ Assets/Scripts/Displays/CharacterDisplay.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDisplay : MonoBehaviour
+public class CharacterDisplay : MonoBehaviour
 {
+    public Character character;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Displays/ItemDisplay.cs
+++ Assets/Scripts/Displays/ItemDisplay.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemDisplay : MonoBehaviour
+public class ItemDisplay : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    public Item item;
+    public InventoryDisplay inventoryDisplay;
+    public Vector3 returnPos; // this is the position the item returns to if it's dropped outside of a drop location
+    public void OnDrag(PointerEventData eventData)
+    {
+        //blocksRaycasts = false;
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //CanvasGroup.blocksRaycasts = true;
+        RectTransform invPanel = inventoryDisplay.transform as RectTransform;
+        if (RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition))
+        {
+            Debug.Log("Drop item");
+            returnPos =inventoryDisplay.Buy(item);
+        }
+        
+        transform.position = returnPos;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        returnPos = transform.position;
     }
 
     // Update is called once per frame
